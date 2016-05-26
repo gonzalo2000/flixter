f917c702d2b4d4652ff688aa767a38449184298a
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-05-26 02:33:34 +0000

    Implement sortable for sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -11,6 +11,11 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render text: 'updated!'
+  end
+
   private
 
     def require_authorized_for_current_course
@@ -21,10 +26,18 @@ class Instructor::SectionsController < ApplicationController
 
     helper_method :current_course
     def current_course
-      @course ||= Course.find(params[:course_id])
+      if params[:course_id]
+        @course ||= Course.find(params[:course_id])
+      else
+        current_section.course
+      end
+    end
+
+    def current_section
+      @section ||= Section.find(params[:id])
     end
 
     def section_params
-      params.require(:section).permit(:title)
+      params.require(:section).permit(:title, :row_order_position)
     end
 end
